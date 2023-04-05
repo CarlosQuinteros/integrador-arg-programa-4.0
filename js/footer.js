01aa6db2ecc933e9c7f38cee80958f8bdee40a78
Carlos Dante <31288453+CarlosQuinteros@users.noreply.github.com>
2023-04-05 09:08:48 -0300

    Se agrego href al id de cada seccion en el footer. Se agrego un metodo jquery en el footer para mostrar el parrafo con id = textoEmpresa

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,5 @@
-console.log("Seccion footer");
+const textoSomos = "Somos una empresa que sabe lo que hace y día a día se esfuerza por hacerlo mejor. Contamos con certificacion ISO 9001 que acreditan la calidad en los procesos de trabajo, buscando mejorar continuamente nuestro desempeño."
+
+;$(document).ready(function () {
+    $('#textoEmpresa').text(textoSomos);
+});
