790cb01dd9c69cbb565677328b43eea310e75747
Carlos Dante <31288453+CarlosQuinteros@users.noreply.github.com>
2023-04-05 21:22:59 -0300

    Se agrego animaciones a los card de equipo

--- js/equipo.js
+++ js/equipo.js
@@ -1,3 +1,4 @@
 $(document).ready(function () {
     $('#textoEquipo').text('Somos un grupo de consultores y programadores expertos en Java, HTML, CSS, Bootstrap, y las más modernas tecnologías listos para enfrentar los proyectos más desafiantes, garantizando la mayor calidad y el cumplimiento de los tiempos pautados.')
+    //$('#equipo article').attr('data-aos','fade-up')
 });
