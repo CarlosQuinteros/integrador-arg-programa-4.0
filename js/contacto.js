e96e4c07ad9f0ce0e5f210e9658ed1150cc0f80d
Carlos Dante <31288453+CarlosQuinteros@users.noreply.github.com>
2023-04-12 12:50:36 -0300

    Se termino seccion de contacto

--- js/contacto.js
+++ js/contacto.js
@@ -2,18 +2,14 @@ const paises = [
     'Brasil',
     'Chile',
     'Colombia',
-    'Costa Rica',
     'Cuba',
     'Ecuador',
-    'El Salvad',
     'Guatemala',
     'onduras',
     'México',
     'Nicaragua',
     'Panamá',
     'Paraguay',
-    'Perú',
-    'Puerto Rico',
     'República',
     'Uruguay',
     'Venezuela'
@@ -23,5 +19,67 @@ $(document).ready(function () {
     paises.forEach(pais => {
         $('#pais').append(`<option value=${pais}>${pais}</option>`)
     })
-    
 });
+
+function enviarFormulario(){
+
+    const formulario = {
+        nombre: $('#nombreInput').val(),
+        apellido: $('#apellidoInput').val(),
+        email: $('#emailInput').val(),
+        pais : $('#pais').val(),
+        mensaje: $('#mensajeInput').val(),
+        sucripto : $('#idSwitchRegistrarme').val()
+    }
+
+    if(formulario.nombre === ''){
+        $('#nombre-error').css("color", "red").text('Debes completar este campo')
+        $('#nombreInput').addClass('is-invalid')
+        return
+    } else{
+        $('#nombre-error').css("color", "green").text('');
+        $('#nombreInput').removeClass('is-invalid').addClass('is-valid')
+    }
+
+    if(formulario.apellido === ''){
+        $('#apellido-error').css("color", "red").text('Debes completar este campo')
+        $('#apellidoInput').addClass('is-invalid')
+        return
+    }else{
+        $('#apellido-error').css("color", "green").text('');
+        $('#apellidoInput').removeClass('is-invalid').addClass('is-valid')
+    }
+
+    if(formulario.email === ''){
+        $('#email-error').css("color", "red").text('Debes completar este campo')
+        $('#emailInput').addClass('is-invalid')
+        return
+    }else{
+        $('#email-error').css("color", "green").text('')
+        $('#emailInput').removeClass('is-invalid').addClass('is-valid')
+    }
+
+    if(formulario.pais === '' || formulario.pais === 'Seleccione un país'){
+        $('#pais-error').css("color", "red").text('Debes completar este campo')
+        $('#pais').addClass('is-invalid')
+        return
+    }else{
+        $('#pais-error').css("color", "green").text('')
+        $('#pais').removeClass('is-invalid').addClass('is-valid')
+    }
+
+    if(formulario.mensaje === ''){
+        $('#mensaje-error').css("color","red").text('Debes completar este campo')
+        $('#mensajeInput').addClass('is-invalid')
+        return
+    }else{
+        $('#mensaje-error').css("color", "green").text('')
+        $('#mensajeInput').removeClass('is-invalid').addClass('is-valid')
+    }
+    //console.log(formulario);
+    Swal.fire({
+        title: `Gracias ${formulario.apellido} ${formulario.nombre}! Recibimos tu mensaje!`,
+        icon:'success',
+        text: `Mensaje Recibido: ${formulario.mensaje}. Te contactaremos a la direccion de correo ${formulario.email}`
+    })
+}
